e5c2ac7a409bda23469b5f65c4a3edd6e3ed7720
krishna <krishnavm143@gmail.com>
2023-05-15 15:00:17 +0530

    Update app.js

--- MaxMillian_Ern/nodejs/node_express/app.js
+++ MaxMillian_Ern/nodejs/node_express/app.js
@@ -4,16 +4,24 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('in Middelware')
-    next()
+// app.use((req, res, next) => {
+//     console.log('in Middelware')
+//     next()
+// })
+
+app.use('/add-product', (req, res, next) => {
+    console.log('in another Middelware')
+    res.send('<h1>In "Add Product" Page</h1>')
 })
 
-app.use((req, res, next) => {
+
+app.use('/', (req, res, next) => {
     console.log('in another Middelware')
     res.send('<h1>Hello from Express</h1>')
 })
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(6000);
+// server.listen(3000);
+
+app.listen(3000)
